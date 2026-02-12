9c342bdba60127d70bbfe8192cf3020f1f7d703a
manarkhalil9 <manarkhalil9@users.noreply.github.com>
2026-02-12 12:39:41 +0300

    css for detil page

--- perla/main_app/views.py
+++ perla/main_app/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render, redirect
-from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Vision, VisionTask, TodoItem
 from .forms import VisionTaskForm
