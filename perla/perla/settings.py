2d8593e99f81d4ce4e0768342b87863cda0e4ef5
manarkhalil9 <manarkhalil9@users.noreply.github.com>
2026-02-12 13:11:35 +0300

    final production settings

--- perla/perla/settings.py
+++ perla/perla/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'dev-secret-key')
 
 DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
 
-    # ⭐ مهم جداً للـ static على Render
     'whitenoise.middleware.WhiteNoiseMiddleware',
 
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -79,7 +78,7 @@ WSGI_APPLICATION = 'perla.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default='postgresql://postgres:postgres@localhost:5432/perla',
+        default='sqlite:///db.sqlite3',
         conn_max_age=600
     )
 }
