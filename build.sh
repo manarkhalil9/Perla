2a05ffbc184ad3b36836cec9e69f3ba179e32f55
manarkhalil9 <manarkhalil9@users.noreply.github.com>
2026-02-12 16:00:47 +0300

    Prepare files for Render deployment

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -o errexit
 
-pip install -r requirements.txt
+pip install -r perla/requirements.txt
 
-python manage.py collectstatic --noinput
+python perla/manage.py collectstatic --no-input
 
-python manage.py migrate
+python perla/manage.py migrate
