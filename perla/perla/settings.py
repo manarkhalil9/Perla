080bc1251d104122eb9e0f3386184d3ec62ed8a8
manarkhalil9 <manarkhalil9@users.noreply.github.com>
2026-02-15 22:02:15 +0300

    Enable media serving for images

--- perla/perla/settings.py
+++ perla/perla/settings.py
@@ -144,6 +144,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
 STORAGES = {
     "staticfiles": {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
