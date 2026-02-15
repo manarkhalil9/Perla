3a69d006346cba6bac25f471290371856d10f357
manarkhalil9 <manarkhalil9@users.noreply.github.com>
2026-02-15 12:06:22 +0300

    trying to make deployment work

--- perla/perla/settings.py
+++ perla/perla/settings.py
@@ -85,17 +85,11 @@ WSGI_APPLICATION = 'perla.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-
-if 'RENDER' in os.environ:
-    DATABASES = {
-        'default': dj_database_url.config(conn_max_age=600)
-    }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
+DATABASES = {
+    'default': dj_database_url.config(
+        default='postgresql://postgres:12345678@localhost:5432/Perla',
+        conn_max_age=600
+    )
     }
 
 # Password validation
