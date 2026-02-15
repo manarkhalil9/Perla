fcd5af5fdd4bdb6a4e7311e8fe8ea546b7011af0
manarkhalil9 <manarkhalil9@users.noreply.github.com>
2026-02-15 13:54:05 +0300

    fix: database migrations and allowed hosts

--- perla/build.sh
+++ perla/build.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 set -o errexit
 
+source ./venv/bin/activate
+
 pip install -r requirements.txt
+
+
 python manage.py collectstatic --no-input
+
+
 python manage.py migrate
--- perla/perla/settings.py
+++ perla/perla/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['perla-64if.onrender.com', 'localhost', '127.0.0.1']
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
